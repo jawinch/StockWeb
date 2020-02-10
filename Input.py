4f9cde565b7c7ef8b82287e2a59b3054e8785def
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-10 13:31:16 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,3 +1,5 @@
-accountSID = raw_input("Account SID: ") #b
-authToken = raw_input("Authorization Token: ") #b
-phoneNumber = raw_input("Phone Number: ") #b
+import os #b
+
+os.accountSID = raw_input("Account SID: ") #b
+os.authToken = raw_input("Authorization Token: ") #b
+os.phoneNumber = raw_input("Phone Number: ") #b
