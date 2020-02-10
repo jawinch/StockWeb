916ea71ae98b8fe6030778d191a9ff492be854ee
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-10 13:24:42 -0800

    Update MagicHappens.py

--- MagicHappens.py
+++ MagicHappens.py
@@ -4,8 +4,9 @@ import Requests #E
 link = requests.get('https://www.nasdaq.com/market-activity/stocks/tsla') #E
 
 #class of current stock price: "symbol-page-header__pricing-price"
+# @Ethan, rewrite using beautiful soup stuff, this might also be complete bs and I don't know what I'm talking about
 def handle_starttag(tag,class,attrs):
-  if tag == "meta" and class == "symbol-page-header__pricing-price":
+  if tag == "span" and class == "symbol-page-header__pricing-price":
     currentValue = class.contents
 
 def main():
