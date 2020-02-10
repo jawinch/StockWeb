5d0674347ea5df1b12c2d90d4316489474347502
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-10 13:04:53 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,16 +1,3 @@
-accountSID = raw_input("Account SID: ")
-authToken = raw_input("Authorization Token: ")
-phoneNumber = raw_input("Phone Number: ")
-
-import urllib.request 
-#basil
-
-def main():
-  webUrl = urllib.request.urlopen("https://www.nasdaq.com/market-activity/stocks/tsla")
-  
-  # read the data from the URL and print it
-  data = webUrl.read()
-
-if __name__ == "__main__":
-  main() 
-#basil
+accountSID = raw_input("Account SID: ") #b
+authToken = raw_input("Authorization Token: ") #b
+phoneNumber = raw_input("Phone Number: ") #b
