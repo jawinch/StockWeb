1e98c6c1ad268342b87521e7dcefb3500be4b9d8
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-10 12:55:56 -0800

    Update MagicHappens.py

--- MagicHappens.py
+++ MagicHappens.py
@@ -1,1 +1,13 @@
+def main():
+  # instantiate the parser and feed it some HTML
+  parser = MyHTMLParser()
+    
+  # open the sample HTML file and read it
+  f = open("samplehtml.html")
+  if f.mode == "r":
+    contents = f.read() # read the entire file
+    parser.feed(contents)
 
+if __name__ == "__main__":
+  main();
+#basil
