9441fe4e014f2e949704f3eab8f3df53ac9529b5
jawinch <33964419+jawinch@users.noreply.github.com>
2020-02-10 13:26:22 -0800

    Twillio out put
    Jackson Winchester

--- Output.py
+++ Output.py
@@ -1,1 +1,10 @@
+import os
+from twilio.rest import CLient
 
+client = Client(account_sid, auth_token)
+if (percent_difference > 5): 
+  client.messages.create(
+   to = os.environ["phone"],
+   from = "+18587077533"
+   body = "The percent difference in stock since " + %d + " is " + %d + "%" % (time_previous, percent_difference) #first %d is time previous second is percent difference
+)
