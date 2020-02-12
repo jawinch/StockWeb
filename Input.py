8965aefab5af5aae48607ce4531bbfc5b9687c46
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-12 12:44:39 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,7 +1,7 @@
 def main():
-  accountSID = raw_input("Account SID: ") #b
-  authToken = raw_input("Authorization Token: ") #b
-  phoneNumber = raw_input("Phone Number: ") #b
+  global accountSID = raw_input("Account SID: ") #b
+  global authToken = raw_input("Authorization Token: ") #b
+  global phoneNumber = raw_input("Phone Number: ") #b
     
 
 if __name__ == "__main__":
