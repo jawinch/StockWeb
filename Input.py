d62feae09d55bbec4ec34635646cc4aa86594975
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2020-02-12 13:36:27 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,8 +1,8 @@
 def main():
-  global accountSID = raw_input("Account SID: ") #b
-  global authToken = raw_input("Authorization Token: ") #b
-  global phoneNumber = raw_input("Phone Number: ") #b
-  # global link = raw_input("NASDAQ Website Link: ") #E 
+  accountSID = raw_input("Account SID: ") #b
+  authToken = raw_input("Authorization Token: ") #b
+  phoneNumber = raw_input("Phone Number: ") #b
+  link = raw_input("NASDAQ Website Link: ") #E 
 
 if __name__ == "__main__":
   main();
