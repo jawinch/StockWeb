f55e5a44fdffb70360e445f9b6e4b9069d15e76b
Ethan Wentworth <43764998+AwesomeSamIAm@users.noreply.github.com>
2020-02-14 11:55:07 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -1,4 +1,9 @@
 def main():
+ # global accountSID
+ # global authToken
+ # global phoneNumber
+ # global link
+  
   accountSID = raw_input("Account SID: ") #b
   authToken = raw_input("Authorization Token: ") #b
   phoneNumber = raw_input("Phone Number: ") #b
