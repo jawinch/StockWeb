d16646abaa27b4ebc69a04506897316041295768
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-19 13:37:58 -0800

    Update Input.py

--- Input.py
+++ Input.py
@@ -7,26 +7,25 @@ def main():
 	authToken = input("Authorization Token: ") #b
 
 	phoneNumber = input("Phone Number: ") #b
+	twilioPhone = input("Twilio Number: ")
 	while (len(phoneNumber) != 10 or not phoneNumber.isdigit()):
   		print("Phone number should be 10 numbers")
   		phoneNumber = input("Phone Number: ") #b
-
-  	twilioPhone = input("Twilio Number: ")
-  	while (len(twilioPhone) != 10 or not twilioPhone.isdigit()):
+	while (len(twilioPhone) != 10 or not twilioPhone.isdigit()):
   		print("Twilio number should be 10 numbers")
   		twilioPhone = input("Twilio Number: ") #b
 
-  	link = input("NASDAQ Website Link: ") #E 
+	link = input("NASDAQ Website Link: ") #E 
 
 	client = Client(accountSID, authToken) #j
 	message = client.messages.create( #j
   		to = phoneNumber, #j
   		from_ = twilioPhone, #j
-  		body = "Is this the correct phone number?") #j
+  		body = "Did you ever hear the tragedy of Darth Plagueis?(Look at command prompt)") #j
 	print(message.sid)# j
 
 	messageReceived = input("Fear leads to anger. Anger leads to hate. Did you receive this message?('yes' or 'no')") #b
-	
+
 	def ask(): #b
   		if (messageReceived == "yes"): #b
   			print("Thanks for using the sytem.") #b
