5a85fcb4ca05fa634b08f01552b5af073a55c17b
basilstein <44881303+basilstein@users.noreply.github.com>
2020-02-21 13:08:17 -0800

    Update Output.py

--- Output.py
+++ Output.py
@@ -4,8 +4,8 @@ from input.py import accountSID
 from input.py import authToken
 from input.py import to
 from input.py import from_
-#from MagicHappens.py import timePrevious
-#from MagicHappens.py import percentDifference
+from MagicHappens.py import timePrevious
+from MagicHappens.py import percentDifference
 
 percentDifference = 5
 timePrevious = "15:30"
@@ -13,8 +13,8 @@ timePrevious = "15:30"
 client = Client(accountSid, authToken)
 
 message = client.messages.create(
-    to = "+15039533843", #os.environ["phone"],
-    from_= "+18587077533",
+    to = to,
+    from_= from_,
     body = "The percent difference in stock since " + %s + " is " + %d + "%" % (time_previous, percent_difference) #first %s is time previous second is percent difference
 )
 if (percentDifference >= 5):
