3a92dc1c9771867d80399d61b539d054e0ca16ac
jawinch <33964419+jawinch@users.noreply.github.com>
2020-03-08 23:47:50 -0700

    Updating website
    g

---
+++ StockWeb-master/Input.py
@@ -0,0 +1,38 @@
+from twilio.rest import Client
+
+def main():
+  
+	accountSID = input("Account SID: ") #b
+
+	authToken = input("Authorization Token: ") #b
+
+	phoneNumber = input("Phone Number: ") #b
+	twilioPhone = input("Twilio Number: ")
+	while (len(phoneNumber) != 10 or not phoneNumber.isdigit()):
+  		print("Phone number should be 10 numbers")
+  		phoneNumber = input("Phone Number: ") #b
+	while (len(twilioPhone) != 10 or not twilioPhone.isdigit()):
+  		print("Twilio number should be 10 numbers")
+  		twilioPhone = input("Twilio Number: ") #b
+
+	link = input("NASDAQ Website Link: ") #E 
+
+	client = Client(accountSID, authToken) #j
+	message = client.messages.create( #j
+  		to = phoneNumber, #j
+  		from_ = twilioPhone, #j
+  		body = "Did you ever hear the tragedy of Darth Plagueis?(Look at command prompt)") #j
+	print(message.sid)# j
+
+	messageReceived = input("Fear leads to anger. Anger leads to hate. Did you receive this message?('yes' or 'no')") #b
+
+	def ask(): #b
+  		if (messageReceived == "yes"): #b
+  			print("Thanks for using the sytem.") #b
+  		elif (messageReceived == "no"): #b
+  			main() #b
+  		else: #b
+  			ask() #b
+
+if __name__ == "__main__":
+	main();
